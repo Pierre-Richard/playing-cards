ef9ff383d33d3b3b2ca4405431dca6252e57c680
Pierre-Richard <mayer_alexandre@yahoo.fr>
2024-09-22 23:07:46 +0200

    created list of monster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, model, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { MonsterType } from './utils/monster.utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlayingCardComponent, SearchBarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PlayingCardComponent,
+    SearchBarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  monsters: Monster[];
-  search = '';
-  count: number = 0;
-  selectedMonsterIndex = signal(1);
+  monsters!: Monster[];
+  search = model('');
+
   constructor() {
     this.monsters = [];
 
     const monster1 = new Monster();
     monster1.name = 'Pick';
     monster1.hp = 40;
-    monster1.figureCaption = 'N°002 Pik';
+    monster1.figureCaption = 'N°001 Pik';
     this.monsters.push(monster1);
 
     const monster2 = new Monster();
@@ -31,15 +36,23 @@ export class AppComponent {
     monster2.image = 'assets/img/pokemonees-689803_640.png';
     monster2.type = MonsterType.WATER;
     monster2.hp = 60;
-    monster2.figureCaption = 'N°003 ar';
+    monster2.figureCaption = 'N°002 ar';
     this.monsters.push(monster2);
-  }
-  increaseCount(): number {
-    return this.count++;
-  }
-  toggleMonster() {
-    this.selectedMonsterIndex.set(
-      (this.selectedMonsterIndex() + 1) % this.monsters.length
-    );
+
+    const monster3 = new Monster();
+    monster3.name = 'Buld';
+    monster3.image = 'assets/img/pixel-3316924_640.png';
+    monster3.type = MonsterType.PLANT;
+    monster3.hp = 60;
+    monster3.figureCaption = 'N°003 Buld';
+    this.monsters.push(monster3);
+
+    const monster4 = new Monster();
+    monster4.name = 'Sala';
+    monster4.image = 'assets/img/pokemon-6895600_640.png';
+    monster4.type = MonsterType.FIRE;
+    monster4.hp = 80;
+    monster4.figureCaption = 'N°004 ar';
+    this.monsters.push(monster4);
   }
 }
