06ea331710435e88b337dc4331374f5e029be165
Pierre-Richard <mayer_alexandre@yahoo.fr>
2024-09-22 22:32:17 +0200

    use signal, model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/monster.model';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { MonsterType } from './utils/monster.utils';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +13,33 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  toggleMonster() {
-    throw new Error('Method not implemented.');
-  }
-  monster1!: Monster[];
+  monsters: Monster[];
   search = '';
   count: number = 0;
-  selectedMonsterIndex = 0;
+  selectedMonsterIndex = signal(1);
   constructor() {
-    this.monster1 = new Monster();
-    this.monster1.name = 'Pick';
-    this.monster1.hp = 40;
-    this.monster1.figureCaption = 'N°002 Pik';
+    this.monsters = [];
+
+    const monster1 = new Monster();
+    monster1.name = 'Pick';
+    monster1.hp = 40;
+    monster1.figureCaption = 'N°002 Pik';
+    this.monsters.push(monster1);
+
+    const monster2 = new Monster();
+    monster2.name = 'Car';
+    monster2.image = 'assets/img/pokemonees-689803_640.png';
+    monster2.type = MonsterType.WATER;
+    monster2.hp = 60;
+    monster2.figureCaption = 'N°003 ar';
+    this.monsters.push(monster2);
   }
   increaseCount(): number {
     return this.count++;
+  }
+  toggleMonster() {
+    this.selectedMonsterIndex.set(
+      (this.selectedMonsterIndex() + 1) % this.monsters.length
+    );
   }
 }
--- src/app/models/monster.model.ts
+++ src/app/models/monster.model.ts
@@ -1,8 +1,13 @@
+import { MonsterType } from '../utils/monster.utils';
+
 export class Monster {
-   name: string = "My Monster";
-   hp: number = 40;
-   figureCaption: string = "N°001 Monster";
-   attackName: string = "Geo Impact";
-   attackStrength: number = 60;
-   attackDescription: string = "This is a long description..."
+  name: string = 'My Monster';
+  image: string = 'assets/img/pokemon-5426712_640.png';
+  type: MonsterType = MonsterType.ELECTRIC;
+  hp: number = 40;
+  figureCaption: string = 'N°001 Monster';
+
+  attackName: string = 'Geo Impact';
+  attackStrength: number = 60;
+  attackDescription: string = 'This is a long description...';
 }
